fd1da05ec810228df8d5e490f9cf0e8cea0a4fbb
Kevin Bris <55791683+kebris-c@users.noreply.github.com>
2025-11-17 20:22:35 +0100

    Add files via upload

--- include/banner.h
+++ include/banner.h
@@ -6,7 +6,7 @@
 /*   By: kebris-c <kebris-c@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/03 12:17:45 by kebris-c          #+#    #+#             */
-/*   Updated: 2025/11/03 17:04:09 by kebris-c         ###   ########.fr       */
+/*   Updated: 2025/11/17 19:09:15 by kebris-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,14 +31,16 @@ typedef struct s_width
 	int	chr;
 }	t_width;
 
+void		ft_banner(const char *str);
 void		free_font_dict(t_font_dict *dict);
-t_font_dict	*new_font_node(char c, char **lines);
 
-void		print_banner(const char *str);
+t_font_dict	*new_font_node(char c, char **lines);
 t_font_dict	*create_font_dict(void);
+
 t_font_dict	*append_digits(t_font_dict *head);
 t_font_dict	*node_2_to_5(char digit);
 t_font_dict	*node_6_to_9(char digit);
+
 t_font_dict	*append_letters(t_font_dict *head);
 t_font_dict	*node_c_to_f(char letter);
 t_font_dict	*node_g_to_j(char letter);
@@ -46,7 +48,7 @@ t_font_dict	*node_k_to_n(char letter);
 t_font_dict	*node_o_to_r(char letter);
 t_font_dict	*node_s_to_v(char letter);
 t_font_dict	*node_w_to_z(char letter);
+
 t_font_dict	*node_owl_(void);
-t_font_dict	*create_font_dict(void);
 
 #endif
---
+++ src/ft_banner.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_banner.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kebris-c <kebris-c@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/30 12:35:54 by kebris-c          #+#    #+#             */
+/*   Updated: 2025/11/17 20:12:59 by kebris-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "banner.h"
+/* ex:
+	██████╗ ██╗   ██╗███████╗██╗  ██╗       ███████╗██╗    ██╗ █████╗ ██████╗
+	██╔══██╗██║   ██║██╔════╝██║  ██║       ██╔════╝██║    ██║██╔══██╗██╔══██╗
+	██████╔╝██║   ██║███████╗███████║       ███████╗██║ █╗ ██║███████║██████╔╝
+	██╔═══╝ ██║   ██║╚════██║██╔══██║       ╚════██║██║███╗██║██╔══██║██╔═══╝
+	██║     ╚██████╔╝███████║██║  ██║██████╗███████║╚███╔███╔╝██║  ██║██║
+	╚═╝      ╚═════╝ ╚══════╝╚═╝  ╚═╝╚═════╝╚══════╝ ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝
+*/
+
+static t_font_dict	*find_ch(t_font_dict *dict, char c)
+{
+	if (c >= 'a' && c <= 'z')
+		c -= 32;
+	while (dict)
+	{
+		if (dict->ch == c)
+			return (dict);
+		dict = dict->next;
+	}
+	return (NULL);
+}
+
+static void	print_font(const char *text)
+{
+	t_font_dict	*dict;
+	t_font_dict	*node;
+	t_font_dict	*owl;
+	int			i;
+	int			line;
+
+	dict = create_font_dict();
+	if (!dict)
+		return ;
+	owl = node_owl_();
+	if (!owl)
+		return ;
+	line = 0;
+	while (line++ < 6)
+	{
+		ft_printf(YELLOW "%s " CYAN, owl->lines[line - 1]);
+		i = 0;
+		while (text[i])
+		{
+			node = find_ch(dict, text[i++]);
+			if (node)
+				ft_printf("%s ", node->lines[line - 1]);
+			else
+				ft_printf("      ");
+		}
+		ft_printf("\n");
+	}
+	free_font_dict(dict);
+}
+
+void	ft_banner(const char *text)
+{
+	ft_printf(CYAN BG_MAGENTA "\n");
+	ft_printf("By: " YELLOW BOLD UNDERLINE "kebris-c\n\n" RESET);
+	ft_printf(CYAN BOLD BG_MAGENTA);
+	print_font(text);
+	ft_printf(RESET);
+}
